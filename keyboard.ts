48aa7407f37ee0519f3c8974e93313abfe8092fd
Mateusz Sowiński <msowinski@sevillemore.com>
2020-11-08 12:40:16 +0100

    Add keyboard tool.

---
+++ keyboard.ts
@@ -0,0 +1,15 @@
+/**
+ * KeyCode enum to use in for example if's
+ * 
+ */
+
+export enum KeyCode {
+    Backspace = 8,
+    Left = 37,
+    Up = 38,
+    Right = 39,
+    Down = 40,
+    Escape = 27,
+    Enter = 13,
+    Shift = 16
+}
