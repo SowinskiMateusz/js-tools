06eea79635627d42c251ceee4ffe43dc735d29d7
Mateusz Sowiński <msowinski@sevillemore.com>
2020-11-08 12:25:05 +0100

    Add email checker.

---
+++ emails.ts
@@ -0,0 +1,7 @@
+/**
+ * Validates email
+ * @param text 
+ */
+
+const EmailRegExp = new RegExp(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
+export const EmailChecker = (text: string) => !!text.match(EmailRegExp);
